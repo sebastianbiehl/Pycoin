0158df97aff651abbe2c1eaceb2510103f0d9755
Sebastian Biehl <sebastian-biehl@gmx.net>
2018-09-18 17:23:07 +0200

    Refactored get_balance function and balance output

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,5 @@
+import functools
+
 # Initializing the blockchain
 
 MINING_REWARD = 10
@@ -23,16 +25,12 @@ def get_balance(participant):
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum, tx_sender, 0)
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant]
                     for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else tx_sum, tx_recipient, 0)
     return amount_received - amount_sent
 
 
@@ -176,7 +174,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid blockchain!')
         break
-    print(get_balance('Sebastian'))
+    print(f'Balance of {owner}: {get_balance(owner):6.2f}')
 
 
 print('Done!')
